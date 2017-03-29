026129a878d735973e69d81564e3cc6c6c6cb4ef
@annettefo <annettefonta@gmail.com>
2017-03-29 14:46:12 -0700

    oop

---
+++ refresher_python/oop.py
@@ -0,0 +1,10 @@
+import random
+import sys
+import os as
+
+#OBJECT ORIENTED PROGRAMMING, ATTRIBUTES. CLASSES
+
+class Animal:
+## __ means this is private --if we change, use function inside class or if we need to use outside the class need to define function
+    __name = None # or "" or None
+    __height = 0
